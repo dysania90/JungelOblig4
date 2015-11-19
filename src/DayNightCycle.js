aa537d77c2debb6f4f2019be9ea0872ada442245
KarianneMoen <kariannemoen@hotmai.com>
2015-11-19 14:26:45 +0100

    lyd

--- src/DayNightCycle.js
+++ src/DayNightCycle.js
@@ -3,17 +3,15 @@
  */
 DayNightCycle = {};
 
-// Need to find a way to make movement of light work
-
 ///////////////////////////////////////////////
 //          Sunlight and movement           //
 /////////////////////////////////////////////
 
-DayNightCycle.sunLight = function  (){
-    var light = new THREE.DirectionalLight(0xf5a914, 2.0); //colour of the sun
-    this.object3d	= light;
+DayNightCycle.sunLight = function() {
+    var light = new THREE.DirectionalLight(0xf5a914, 2.0); //color of the sun
+    this.object3d = light;
 
-    this.update	= function(sunAngle){
+    this.update	= function(sunAngle) {
         light.position.x = 0;
         light.position.y = Math.sin(sunAngle) * 10000;
         light.position.z = -Math.cos(sunAngle) * 10000;
@@ -22,30 +20,42 @@ DayNightCycle.sunLight = function  (){
 };
 
 /////////////////////////////////////////////
-//              Sun object                //
-///////////////////////////////////////////
-
-DayNightCycle.sunObject = function () {
-    var sunGeometry	= new THREE.SphereGeometry( 200, 300, 300 );
-    var sunMaterial	= new THREE.MeshPhongMaterial({
-        color		: 0xff0000
-    });
-    var sunMesh	= new THREE.Mesh(sunGeometry, sunMaterial);
-    this.object3d	= sunMesh;
-
-    this.update	= function(sunAngle) {
-        mesh.position.x = 0;
-        mesh.position.y = Math.sin(sunAngle) * 10000;
-        mesh.position.z = -Math.cos(sunAngle) * 10000;
-    }
-
-
-};
-
-/////////////////////////////////////////////
 //                 Skybox                 //
 ///////////////////////////////////////////
 
-DayNightCycle.Skybox = function () {
+DayNightCycle.skybox = function () {
+    var urlPrefix;
+    var urls;
+    var textureCube;
+    var shader;
+    var skyBoxMaterial;
+    var mat;
+    var skyBox;
 
+    urlPrefix = 'textures/';
+    urls = [ urlPrefix + 'sea_negx.jpg',
+        urlPrefix + 'sea_posx.jpg',
+        urlPrefix + 'sea_negy.jpg',
+        urlPrefix + 'sea_posy.jpg',
+        urlPrefix + 'sea_negz.jpg',
+        urlPrefix + 'sea_posz.jpg' ];
+
+    textureCube = THREE.ImageUtils.loadTextureCube(urls);
+    textureCube.format = THREE.RGBFormat;
+    shader = THREE.ShaderLib['cube']; // init cube shader from built-in lib
+    shader.uniforms['tCube'].value = textureCube; // apply textures to shader
+
+    // create shader material
+    skyBoxMaterial = new THREE.ShaderMaterial({
+        fragmentShader : shader.fragmentShader,
+        vertexShader : shader.vertexShader,
+        uniforms : shader.uniforms,
+        depthWrite : false,
+        side : THREE.BackSide
+    });
+    mat = new THREE.MeshBasicMaterial({envMap: textureCube, side: THREE.BackSide});
+    // create skybox mesh
+    skyBox = new THREE.Mesh(new THREE.BoxGeometry(300000, 400000, 300000), mat);
+    skyBox.doubleSided = true;
+    groundMesh.add(skyBox);
 };
